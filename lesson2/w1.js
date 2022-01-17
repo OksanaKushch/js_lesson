3ee7110b0f740c09b5368a890b305d6640f273ca
OksanaKushch <oksana.kusc@gmail.com>
2022-01-17 17:30:04 +0200

    lesson2

--- lesson2/w1.js
+++ lesson2/w1.js
@@ -1,3 +1,14 @@
+/*
+Задача 1
+Перепишите код, заменив оператор `if` на тернарный оператор `?`
+```js
+var result;
+if (a + b < 4) {
+result = true;
+} else {
+result = false;
+}
+*/
 
 var result;
 (a + b < 4) ? result = true: result = false;
--- lesson2/w4.js
+++ lesson2/w4.js
@@ -1,3 +1,19 @@
+
+/*Задача 4
+Перепишите код с использованием одной конструкции `switch`:
+```js
+var a = 0;
+
+if (a == 0) {
+console.log( 0 );
+}
+if (a == 1) {
+console.log( 1 );
+}
+if (a == 2 || a == 3) {
+console.log( '2,3' );
+}
+ */
 var a = 0;
 switch (a) {
 case 0:
