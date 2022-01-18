cd60266a3a9dfad55032cc64031cd648389de6d7
OksanaKushch <97605322+OksanaKushch@users.noreply.github.com>
2022-01-18 17:07:31 +0300

    les

--- lesson2/w10.js
+++ lesson2/w10.js
@@ -9,9 +9,9 @@ var arr1;
 for (let i = 0; i < arr.length-1; i++) {
  for (let j = i+1; j < arr.length; j++){
  if (arr[i] > arr[j]) {
-   arr1 == arr[i];
-   arr[i] == arr[j];
-   arr[j] == arr1;
+   arr1 = arr[i];
+   arr[i] = arr[j];
+   arr[j] = arr1;
  };
  };
  };
