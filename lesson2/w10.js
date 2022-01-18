b2592d7b1f3dba7e043b5263dd6ffbaa8d6b0be4
OksanaKushch <97605322+OksanaKushch@users.noreply.github.com>
2022-01-18 14:38:03 +0300

    les

--- lesson2/w10.js
+++ lesson2/w10.js
@@ -6,8 +6,8 @@ var arr = [6,5,4,3,2,1];
 ``` */
 var arr = [6,5,4,3,2,1];
 var arr1;
-for (i = 0; i < arr.length-1; i++) {
- for (j = i+1; j < arr.length; j++){
+for (let i = 0; i < arr.length-1; i++) {
+ for (let j = i+1; j < arr.length; j++){
  if (arr[i] > arr[j]) {
    arr1 == arr[i];
    arr[i] == arr[j];
