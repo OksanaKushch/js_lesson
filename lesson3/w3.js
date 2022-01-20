ba3688c0e0f11e9db3a80b25f9f997226dcaa315
OksanaKushch <oksana.kusc@gmail.com>
2022-01-20 12:01:38 +0200

    les3

---
+++ lesson3/w3.js
@@ -0,0 +1,21 @@
+/*Задача 3
+
+Сделайте функцию `f`, которая отнимает от первого числа второе и делит на третье.
+Данная функция должна обязательно содержать проверку входных параметров,
+потому что принимать она может только числа.
+
+```js
+f(9,3,2); // 3
+f('s',9,3) // Error: all parameters type should be a Number
+```
+ */
+function f(a, b, c) {
+    let result;
+    if (typeof a === 'number' && typeof b === 'number' && typeof c === 'number'){
+        result = (a-b)/c;
+    } else {
+        throw new Error('Error: all parameters type should be a Number')
+    }
+    return result;
+};
+
