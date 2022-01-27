4c6840ed044126534fdcbbf4ec05cce6033f02ee
OksanaKushch <oksana.kusc@gmail.com>
2022-01-27 23:46:50 +0200

    les4

--- lesson4/w5.js
+++ lesson4/w5.js
@@ -13,4 +13,20 @@ const arr = [1,2,3];
 const acc = 0;
 reduce(arr, function(acc, item, i, arr) {}, acc);
 ```*/
-
+function reduce(arr, f, acc ){
+    if (!Array.isArray(arr)){ 
+        throw new Error('Первый параметр обязателен и может принимать только массив')}
+        if (typeof f != "function"){ 
+            throw new Error('Второй параметр обязателен и может принимать только функцию')}
+            if (typeof acc != ("number" || "string")  ){ 
+                throw new Error('Третий параметр обязателен и может принимать только строку или число')}
+                                for (let i = 0; i < arr.length; i++) {
+                       f(acc, arr[i], i, arr);
+                       acc = acc + arr[i]
+                    }
+                    return acc;
+                  };
+            
+const arr = [1,2,3];
+const acc = 0;
+console.log(reduce(arr, function(acc, item, i, arr){console.log(acc)}, acc))
