c1d91e4931a7cb6f703b6ca329bc17396ffd5a01
OksanaKushch <oksana.kusc@gmail.com>
2022-01-27 09:16:19 +0200

    Update w9.js
    еуіе

---
+++ lesson3/w9.js
@@ -0,0 +1,16 @@
+let a=[1,2,3,4,5];
+let b=[1,2,3,4];
+
+function solution(a, b) {
+    let с=[];
+    if (a.length > b.length){
+        for(let i=a.length-1; i>0;i--)
+        {
+            for(let j=b.length-1; j>0;j--){c[i]=b[j]}; console.log(c[i]);}
+}
+
+
+}
+
+
+solution(a, b)
