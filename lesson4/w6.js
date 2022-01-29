1d941861b23a1bacafcd26f50b64a6ca1bf1306e
OksanaKushch <oksana.kusc@gmail.com>
2022-01-29 22:41:32 +0200

    les4

--- lesson4/w6.js
+++ lesson4/w6.js
@@ -18,7 +18,7 @@ function reduceRight(arr, f, acc ){
         throw new Error('Первый параметр обязателен и может принимать только массив')}
         if (typeof f != "function"){ 
             throw new Error('Второй параметр обязателен и может принимать только функцию')}
-            if (typeof acc != ("number" || "string")  ){ 
+            if (typeof(acc) !== "number" && typeof(acc) !== "string"  ){ 
                 throw new Error('Третий параметр обязателен и может принимать только строку или число')}
                                 for (let i = arr.length-1; i >= 0; i--) {
                        f(acc, arr[i], i, arr);
@@ -29,4 +29,4 @@ function reduceRight(arr, f, acc ){
             
 const arr = [1,2,3];
 const acc = 0;
-console.log(reduceRight(arr, function(acc, item, i, arr){console.log(acc)}, acc))
+console.log(reduceRight(arr, function(acc, item, i, arr){acc}, acc))
